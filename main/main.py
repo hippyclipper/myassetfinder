1bdd354796f28a9159bde69761e636670c90e71c
hippyclipper <cameron_moon321@hotmail.com>
2021-04-12 21:06:55 +0000

    working

--- main/main.py
+++ main/main.py
@@ -2,49 +2,79 @@ import sys
 import requests
 
 def waybacksubs(domain):
+    ret = set({})
     url = "http://web.archive.org/cdx/search/cdx?url=*.{domain}/*&output=json&collapse=urlkey".format(domain = domain)
     r = requests.get(url)
     jsonReq = r.json()
     for line in jsonReq[1:]:
         sub = ".".join(line[0].split(")/")[0].split(",")[::-1])
-        print(sub)
+        ret.add(sub)
+    return ret
 
 def hackertarget(domain):
+    ret = set({})
     url = "https://api.hackertarget.com/hostsearch/?q={domain}".format(domain = domain)
     r = requests.get(url)
     textReq = r.text.splitlines()
     for line in textReq:
-        print(line.split(",")[0])
+        ret.add(line.split(",")[0])
+    return ret
 
 
 def bufferover(domain):
+    ret = set({})
     url = "https://dns.bufferover.run/dns?q=.{domain}".format(domain=domain)
     r = requests.get(url)
     jsonReq = r.json()
     for line in jsonReq["RDNS"]:
-        print(line.split(",")[1])
-
+        ret.add(line.split(",")[1])
+    return ret
 
 def urlscan(domain):
+    ret = set({})
     url = "https://urlscan.io/api/v1/search/?q=domain:{domain}".format(domain = domain)
     r = requests.get(url)
     jsonReq = r.json()
     for output in jsonReq["results"]:
-        print(output["task"]["domain"])
+        ret.add(output["task"]["domain"])
+    return ret
 
 def crtsh(domain):
+    ret = set({})
     r = requests.get("https://crt.sh/?q=%.{domain}&output=json".format(domain = domain))
     jsonReq = r.json()
     for outputs in jsonReq:
-        print(outputs["name_value"])
+        ret.add(outputs["name_value"])
+    return ret
 
 def main():
     domain = sys.argv[1]
-    crtsh(domain)
-    urlscan(domain)
-    bufferover(domain)
-    hackertarget(domain)
-    waybacksubs(domain)
+    finalSet = set({})
+    try:
+        finalSet.update(crtsh(domain))
+    except Exception as e:
+        pass
+    try:
+        finalSet.update(urlscan(domain))
+    except Exception as e:
+        pass
+    try:
+        finalSet.update(bufferover(domain))
+    except Exception as e:
+        pass
+    try:
+        finalSet.update(hackertarget(domain))
+    except Exception as e:
+        pass
+    try:
+        finalSet.update(waybacksubs(domain))
+    except Exception as e:
+        pass
+    for d in finalSet:
+        if (not d.startswith("*.")) and domain in d:
+            print(d)
+        
+       
         
 
 
