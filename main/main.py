f369a11f736e25dda2b1132d22c4bb5d7cdfa02c
hippyclipper <cameron_moon321@hotmail.com>
2022-05-08 11:42:15 -0700

    removed dev files, put functions in a list

--- main/main.py
+++ main/main.py
@@ -1,6 +1,9 @@
 import sys
 import requests
 
+#to any future users.
+#follow the tos of any included API's
+
 def waybacksubs(domain):
     ret = set({})
     url = "http://web.archive.org/cdx/search/cdx?url=*.{domain}/*&output=json&collapse=urlkey".format(domain = domain)
@@ -34,7 +37,7 @@ def urlscan(domain):
     ret = set({})
     url = "https://urlscan.io/api/v1/search/?q=domain:{domain}".format(domain = domain)
     r = requests.get(url)
-    jsonReq = r.json()
+    jsonReq = r.json() 
     for output in jsonReq["results"]:
         ret.add(output["task"]["domain"])
     return ret
@@ -42,40 +45,31 @@ def urlscan(domain):
 def crtsh(domain):
     ret = set({})
     r = requests.get("https://crt.sh/?q=%.{domain}&output=json".format(domain = domain))
-    jsonReq = r.json()
+    jsonReq = r.json()   
     for outputs in jsonReq:
         ret.add(outputs["name_value"])
     return ret
 
 def main():
-    domain = sys.argv[1]
+    
+    domain = ""
+    
+    if len(sys.argv) > 1:
+        domain = sys.argv[1]
+    else:
+        domain = input("domain: ")
+
     finalSet = set({})
-    try:
-        finalSet.update(crtsh(domain))
-    except Exception as e:
-        pass
-    try:
-        finalSet.update(urlscan(domain))
-    except Exception as e:
-        pass
-    try:
-        finalSet.update(bufferover(domain))
-    except Exception as e:
-        pass
-    try:
-        finalSet.update(hackertarget(domain))
-    except Exception as e:
-        pass
-    try:
-        finalSet.update(waybacksubs(domain))
-    except Exception as e:
-        pass
+    functions = [crtsh, urlscan, bufferover, hackertarget, waybacksubs]
+    
+    for function in functions:
+        try:
+            finalSet.update(function(domain))
+        except Exception as e:
+            pass
+        
     for d in finalSet:
         if (not d.startswith("*.")) and domain in d:
             print(d)
         
-       
-        
-
-
 main()
